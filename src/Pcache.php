0a3c82cc012d41e2a5672cc06cc0783e8847d47f
Dan Hughes <danhughes.dh+github@gmail.com>
2015-06-13 22:41:09 +0100

    Remove built in timing control

--- src/Pcache.php
+++ src/Pcache.php
@@ -1,16 +1,9 @@
 <?php namespace xes;
 class Pcache {
 	private $predis;
-	private static $times;
 
 	public function __construct(\Predis\Client $predis) {
 		$this->predis = $predis;
-
-		self::$times = [
-			'short' => 0.5*60,
-			'default' => 1*60,
-			'long' => 5*60,
-		];
 	}
 
 	public function get($name, $expiry, callable $callback) {
@@ -20,18 +13,6 @@ class Pcache {
 		}
 
 		return $this->predis->get($name);
-	}
-
-	public static function setTimes($times) {
-		self::$times = $times;
-	}
-
-	public static function expire($lengthName) {
-		foreach(self::$times as $name => $mins) {
-			if ($lengthName == $name) { return $mins; }
-		}
-		throw new Exception("Invalid expiry: $length");
-
 	}
 }
 ?>
