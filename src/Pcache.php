70386f3b8f382062965bc0fc1685e3e5063f6355
Dan Hughes <danhughes.dh+github@gmail.com>
2015-06-14 13:14:21 +0100

    Silently disable self if unable to connect

--- src/Pcache.php
+++ src/Pcache.php
@@ -1,12 +1,23 @@
 <?php namespace xes;
 class Pcache {
 	private $predis;
+	private $enabled = true;
 
 	public function __construct(\Predis\Client $predis) {
 		$this->predis = $predis;
+
+		try {
+			$predis->connect();
+		} catch (\Predis\Connection\ConnectionException $exception) {
+			$this->enabled = false;
+		}
 	}
 
 	public function get($name, $expiry, callable $callback) {
+		if (!$this->enabled) {
+			return $callback();
+		}
+
 		if (!$this->predis->exists($name)) {
 			$this->predis->set($name, $callback());
 			$this->predis->expire($name, $this->randomise($expiry));
