5e83a6936d7a74a2efc4d68d833edbd6065d3592
Dan Hughes <danhughes.dh+github@gmail.com>
2015-06-14 12:14:46 +0100

    Add 10% randomisation to prevent mass cache expiry for single user

--- src/Pcache.php
+++ src/Pcache.php
@@ -9,10 +9,14 @@ class Pcache {
 	public function get($name, $expiry, callable $callback) {
 		if (!$this->predis->exists($name)) {
 			$this->predis->set($name, $callback());
-			$this->predis->expire($name, $expiry);
+			$this->predis->expire($name, $this->randomise($expiry));
 		}
 
 		return $this->predis->get($name);
+	}
+
+	private function randomise($time) {
+		return $time + (mt_rand(-$time/10, $time/10));
 	}
 }
 ?>
